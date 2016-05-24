60a4a7f1b460c179f296a6a205a912f36324b6ec
Nuriddin Tussipov <ntussipov@gmail.com>
2016-05-24 16:45:13 -0500

    refactored API-tool, added Get() method

--- API-tool.js
+++ API-tool.js
@@ -1,39 +1,44 @@
 function AdhObject(collection, getProperties) {
   this.Collection = collection;
-  /*this.Criteria = {},
-  this.Fields =  {},
-  this.fieldname = function(fieldname, val){
-    this.Fields{fieldname,val}
-  }
-  this.save = Save(this.Collection, this.Fields);*/
 
+  this.fieldsToJson = function() {
+    data = {};
+    // loop over the properties and convert them to JSON
+    Object.keys(this).forEach(function(key,index) {
+      if (key != "Collection" && key != "Save" && key != "Get" && key != "fieldsToJson")
+          data[key] = this[key];
+    }.bind(this));
+    return data;
+  }
 
   this.Save = function () {
-    Fields = {}
-    /*for (var property in Object.keys(this)) {
-        if (property != "Collection" && property != "S") {
-            Fields[property] = this[property];
-            console.log(property);
-          }
-    }*/
-
-    // loop over the properties and them to the Fields JSON
-    Object.keys(this).forEach(function(key,index) {
-      if (key != "Collection" && key != "Save") {
-          Fields[key] = this[key];
-        }
-    }.bind(this));
-
+    Fields = this.fieldsToJson();
     // ajax post call
     data = {"Collection": this.Collection, "Fields": Fields};
     $.ajax({
-    dataType: "json",
-    type: "POST",
-    url: "http://localhost:5000/update",
-    data: data,
-    success: function (succ) {
-        console.log(succ);
-    }
-  });
-}
+      dataType: "json",
+      type: "POST",
+      url: "http://localhost:5000/update",
+      data: data,
+      success: function (succ) {
+          console.log(succ);
+      }
+    });
+  }
+  this.Get = function () {
+    obj = {}
+    Criteria = this.fieldsToJson();
+    data={"Collection": this.Collection, "Criteria": Criteria};
+    $.ajax({
+      dataType: "json",
+      async: false,
+      type: "GET",
+      url: "http://localhost:5000/",
+      data: data,
+      success: function (result) {
+        obj = result
+      }
+    });
+    return obj;
+  }
 }
