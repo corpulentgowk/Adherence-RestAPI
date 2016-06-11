b8e4677209b1752df9ce71dff6649a85692b4ba9
Steven Layne <stevenzmanx@u.northwestern.edu>
2016-06-11 15:33:00 -0500

    changes

--- index.js
+++ index.js
@@ -235,6 +235,11 @@ app.post('/update', function(request, response) {
 
 
 })
+app.get ('/getColumnNames', function (request, response) {
+
+}
+}
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
